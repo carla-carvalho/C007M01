7d757894f0af6f5294d6e5a96305b0f1d86db0ef
carla-carvalho <89050567+carla-carvalho@users.noreply.github.com>
2021-08-23 20:24:19 -0300

    Add files via upload
    Projeto realizado com o seguinte enunciado:
    
    Faça um programa que faça 5 perguntas para uma pessoa sobre um crime. As perguntas são:
    "Telefonou para a vítima?"
    "Esteve no local do crime?"
    "Mora perto da vítima?"
    "Devia para a vítima?"
    "Já trabalhou com a vítima?" 
    
    O programa deve no final emitir uma classificação sobre a participação da pessoa no crime. 
    Se a pessoa responder positivamente a 2 questões ela deve ser classificada como "Suspeita", entre 3 e 4 como "Cúmplice" e 5 como "Assassino". Caso contrário, ele será classificado como "Inocente".

---
+++ ProjetoSherlock_Blue.js
@@ -0,0 +1,28 @@
+console.log("Por favor, responda as perguntas abaixo uitilizando 0 para não e 1 para sim");
+
+let primeiraPergunta = +prompt ("Telefonou para a vítima?");
+let segundaPergunta = +prompt ("Esteve no local do crime?");
+let terceiraPergunta = +prompt ("Mora perto da vítima?");
+
+let quartaPergunta = +prompt ("Devia para a vítima?");
+
+let quintaPergunta = +prompt ("Já trabalhou com a vítima?");
+
+if (primeiraPergunta + segundaPergunta + terceiraPergunta + quartaPergunta + quintaPergunta == 5 ) {
+console.log("Você é o assasino")}
+
+ else if  (primeiraPergunta + segundaPergunta + terceiraPergunta + quartaPergunta + quintaPergunta == 4 ) {
+  console.log("Você é cúmplice")}
+
+else if (primeiraPergunta + segundaPergunta + terceiraPergunta + quartaPergunta + quintaPergunta == 3 ) {
+console.log("Você é cúmplice")}
+
+else if  (primeiraPergunta + segundaPergunta + terceiraPergunta + quartaPergunta + quintaPergunta == 2 ){
+  console.log("Você é suspeito")}
+
+  else if  (primeiraPergunta + segundaPergunta + terceiraPergunta + quartaPergunta + quintaPergunta <2 ){
+  console.log("Você é inocente")}
+
+else {
+  console.log ("Opção inválida")
+}
